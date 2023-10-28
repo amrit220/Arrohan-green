214a9304cee86c91ef2c90f0ec640ed656004d48
Amrit Panda <amrithacker20@gmail.com>
2023-10-28 08:58:01 +0530

    updates contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,41 +1,28 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
-
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Replace this email address with your real receiving email address
+    $receiving_email_address = 'amy.captivating@gmail.com';
+    
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    
+    $email_body = "<p><strong>Name:</strong> $name</p>";
+    $email_body .= "<p><strong>Email:</strong> $email</p>";
+    $email_body .= "<p><strong>Message:</strong><br> $message</p>";
+    
+    if (mail($receiving_email_address, $subject, $email_body, $headers)) {
+        echo "Your message has been sent successfully!";
+    } else {
+        echo "Failed to send the message. Please try again later.";
+    }
+} else {
+    echo "Invalid request.";
+}
 ?>
